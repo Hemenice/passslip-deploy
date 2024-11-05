d89976f345c5af1a917721aa3fc31feafd0068e1
Romeo Tangpuz Jimenez <romeotjimenez@gmail.com>
2024-11-06 00:35:37 +0800

    million add

--- app/Http/Controllers/AdminRequestPassSlipController.php
+++ app/Http/Controllers/AdminRequestPassSlipController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Head;
+use Milon\Barcode\DNS1D;
+
 use App\Models\Slip;
 use App\Models\User;
 use App\Models\Purpose;
@@ -49,9 +51,9 @@ class AdminRequestPassSlipController extends Controller
         $slip->control_number = str_pad($slip->id, 8, '0', STR_PAD_LEFT);
 
         // Generate the barcode using the control number
-        $generator = new BarcodeGeneratorPNG();
+        $barcodeGenerator = new DNS1D();
         // $barcodeData = $generator->getBarcode($slip->control_number, $generator::TYPE_CODE_128);
-        $barcodeData = $generator->getBarcode($slip->control_number, $generator::TYPE_CODE_128);
+        $barcodeData = $barcodeGenerator->getBarcodePNG($slip->control_number, 'C128');
 
         // Define the barcode image file name and path
         $barcodeFileName = 'barcode_' . $slip->control_number . '.png';
@@ -59,7 +61,7 @@ class AdminRequestPassSlipController extends Controller
 
         // Save the barcode image to the public directory
         // Storage::disk('public')->put('barcodes/' . $barcodeFileName, $barcodeData);
-        Storage::disk('public')->put('barcodes/' . $barcodeFileName, $barcodeData);
+        Storage::disk('public')->put('barcodes/' . $barcodeFileName, base64_decode($barcodeData));
 
         // Save the barcode path or image name to the database
         $slip->barcode = $barcodeFileName; // Assuming you have a `barcode` column in the `slips` table
--- config/app.php
+++ config/app.php
@@ -123,4 +123,10 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+
+    'providers' => [
+        // ...
+        Milon\Barcode\BarcodeServiceProvider::class,
+    ]
+
 ];
