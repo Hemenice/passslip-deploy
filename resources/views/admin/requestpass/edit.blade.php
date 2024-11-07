3f00f8630b303355e40252636bbe147afef45aaf
Romeo Tangpuz Jimenez <romeotjimenez@gmail.com>
2024-11-08 05:36:32 +0800

    edit

--- resources/views/admin/requestpass/edit.blade.php
+++ resources/views/admin/requestpass/edit.blade.php
@@ -6,248 +6,8 @@
 <body>
 
     <!-- ======= Header ======= -->
-    <header id="header" class="header fixed-top d-flex align-items-center">
+    <x-admin.header />
 
-        <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
-                <img src="../assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block">E-Pass Slip RS</span>
-            </a>
-            <i class="bi bi-list toggle-sidebar-btn"></i>
-        </div><!-- End Logo -->
-
-        {{-- <div class="search-bar">
-         <form class="search-form d-flex align-items-center" method="POST" action="#">
-             <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-             <button type="submit" title="Search"><i class="bi bi-search"></i></button>
-         </form>
-     </div> --}}
-        <!-- End Search Bar -->
-
-        <nav class="header-nav ms-auto">
-            <ul class="d-flex align-items-center">
-
-                <li class="nav-item d-block d-lg-none">
-                    <a class="nav-link nav-icon search-bar-toggle " href="#">
-                        <i class="bi bi-search"></i>
-                    </a>
-                </li><!-- End Search Icon-->
-
-                <li class="nav-item dropdown">
-
-                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                        <i class="bi bi-bell"></i>
-                        <span class="badge bg-primary badge-number">4</span>
-                    </a><!-- End Notification Icon -->
-
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                        <li class="dropdown-header">
-                            You have 4 new notifications
-                            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="notification-item">
-                            <i class="bi bi-exclamation-circle text-warning"></i>
-                            <div>
-                                <h4>Lorem Ipsum</h4>
-                                <p>Quae dolorem earum veritatis oditseno</p>
-                                <p>30 min. ago</p>
-                            </div>
-                        </li>
-
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="notification-item">
-                            <i class="bi bi-x-circle text-danger"></i>
-                            <div>
-                                <h4>Atque rerum nesciunt</h4>
-                                <p>Quae dolorem earum veritatis oditseno</p>
-                                <p>1 hr. ago</p>
-                            </div>
-                        </li>
-
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="notification-item">
-                            <i class="bi bi-check-circle text-success"></i>
-                            <div>
-                                <h4>Sit rerum fuga</h4>
-                                <p>Quae dolorem earum veritatis oditseno</p>
-                                <p>2 hrs. ago</p>
-                            </div>
-                        </li>
-
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="notification-item">
-                            <i class="bi bi-info-circle text-primary"></i>
-                            <div>
-                                <h4>Dicta reprehenderit</h4>
-                                <p>Quae dolorem earum veritatis oditseno</p>
-                                <p>4 hrs. ago</p>
-                            </div>
-                        </li>
-
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li class="dropdown-footer">
-                            <a href="#">Show all notifications</a>
-                        </li>
-
-                    </ul><!-- End Notification Dropdown Items -->
-
-                </li><!-- End Notification Nav -->
-
-                <li class="nav-item dropdown">
-
-                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-                        <i class="bi bi-chat-left-text"></i>
-                        <span class="badge bg-success badge-number">3</span>
-                    </a><!-- End Messages Icon -->
-
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                        <li class="dropdown-header">
-                            You have 3 new messages
-                            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="message-item">
-                            <a href="#">
-                                <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle">
-                                <div>
-                                    <h4>Maria Hudson</h4>
-                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                                    <p>4 hrs. ago</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="message-item">
-                            <a href="#">
-                                <img src="assets/img/messages-2.jpg" alt="" class="rounded-circle">
-                                <div>
-                                    <h4>Anna Nelson</h4>
-                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                                    <p>6 hrs. ago</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="message-item">
-                            <a href="#">
-                                <img src="assets/img/messages-3.jpg" alt="" class="rounded-circle">
-                                <div>
-                                    <h4>David Muldon</h4>
-                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                                    <p>8 hrs. ago</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li class="dropdown-footer">
-                            <a href="#">Show all messages</a>
-                        </li>
-
-                    </ul><!-- End Messages Dropdown Items -->
-
-                </li><!-- End Messages Nav -->
-
-                <li class="nav-item dropdown pe-3">
-
-                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
-                        data-bs-toggle="dropdown">
-                        <img src="../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">
-                            {{ Auth::check() ? Auth::user()->name : 'Guest' }}</span>
-                    </a><!-- End Profile Iamge Icon -->
-
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                        <li class="dropdown-header">
-                            <h6> {{ Auth::check() ? Auth::user()->name : 'Admin' }}</h6>
-                            <span> {{ Auth::check() ? Auth::user()->email : 'Admin' }}</span>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li>
-                            <a class="dropdown-item d-flex align-items-center" href="-profile.html">
-                                <i class="bi bi-person"></i>
-                                <span>My Profile</span>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li>
-                            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-                                <i class="bi bi-gear"></i>
-                                <span>Account Settings</span>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li>
-                            <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
-                                <i class="bi bi-question-circle"></i>
-                                <span>Need Help?</span>
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-
-                        <li>
-                            <form id="logout-form" action="/adminlogout" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                            <a class="dropdown-item d-flex align-items-center" href="#"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                <i class="bi bi-box-arrow-right"></i>
-                                <span>Sign Out</span>
-                            </a>
-                        </li>
-
-
-
-                    </ul><!-- End Profile Dropdown Items -->
-                </li><!-- End Profile Nav -->
-
-            </ul>
-        </nav><!-- End Icons Navigation -->
-
-    </header>
-
-    <!-- End Header -->
-    <!-- Sidebar Here -->
-
-    <!-- ======= Sidebar ======= -->
-    <!-- ======= Sidebar ======= -->
     <x-admin.sidebar />
     <!-- End Sidebar-->
 
@@ -283,9 +43,8 @@
                                 <label for="timeDeparture" class="col-sm-2 col-form-label">Time of
                                     Departure</label>
                                 <div class="col-sm-10">
-                                    <input name="time_departure" type="time" class="form-control"
-                                        id="timeDeparture"
-                                        value="{{ old('time_departure', $requestPass->time_departure) }}" required>
+                                    <input name="time_departure" type="time" class="form-control" id="timeDeparture"
+                                        value="{{ old('time_departure', $requestPass->time_departure) }}">
                                 </div>
                             </div>
 
@@ -294,7 +53,7 @@
                                 <label for="timeArrival" class="col-sm-2 col-form-label">Time of Arrival</label>
                                 <div class="col-sm-10">
                                     <input name="time_arrival" type="time" class="form-control" id="timeArrival"
-                                        value="{{ old('time_arrival', $requestPass->time_arrival) }}" required>
+                                        value="{{ old('time_arrival', $requestPass->time_arrival) }}">
                                 </div>
                             </div>
 
@@ -303,9 +62,8 @@
                                 <label for="dateDeparture" class="col-sm-2 col-form-label">Date of
                                     Departure</label>
                                 <div class="col-sm-10">
-                                    <input name="date_departure" type="date" class="form-control"
-                                        id="dateDeparture"
-                                        value="{{ old('date_departure', $requestPass->date_departure) }}" required>
+                                    <input name="date_departure" type="date" class="form-control" id="dateDeparture"
+                                        value="{{ old('date_departure', $requestPass->date_departure) }}">
                                 </div>
                             </div>
 
@@ -314,7 +72,7 @@
                                 <label for="dateArrival" class="col-sm-2 col-form-label">Date of Arrival</label>
                                 <div class="col-sm-10">
                                     <input name="date_arrival" type="date" class="form-control" id="dateArrival"
-                                        value="{{ old('date_arrival', $requestPass->date_arrival) }}" required>
+                                        value="{{ old('date_arrival', $requestPass->date_arrival) }}">
                                 </div>
                             </div>
 
@@ -322,7 +80,7 @@
                             <div class="row mb-3">
                                 <label for="purpose" class="col-sm-2 col-form-label">Purpose</label>
                                 <div class="col-sm-10">
-                                    <select name="purpose" id="purpose" class="form-control" required>
+                                    <select name="purpose" id="purpose" class="form-control">
                                         <option value="" disabled>Select Purpose</option>
                                         @foreach ($purpose as $purp)
                                             <option value="{{ $purp->purpose_name }}"
@@ -338,7 +96,7 @@
                             <div class="row mb-3">
                                 <label for="status" class="col-sm-2 col-form-label">Status</label>
                                 <div class="col-sm-10">
-                                    <select name="status" id="status" class="form-control" required>
+                                    <select name="status" id="status" class="form-control">
                                         <option value="" disabled>Select Status</option>
                                         <option value="pending"
                                             {{ $requestPass->status == 'pending' ? 'selected' : '' }}>Pending</option>
@@ -357,7 +115,7 @@
                             <div class="row mb-3">
                                 <label for="reason" class="col-sm-2 col-form-label">Reason</label>
                                 <div class="col-sm-10">
-                                    <textarea name="reason" class="form-control" id="reason" rows="3" required>{{ old('reason', $requestPass->reason) }}</textarea>
+                                    <textarea name="reason" class="form-control" id="reason" rows="3">{{ old('reason', $requestPass->reason) }}</textarea>
                                 </div>
                             </div>
 
@@ -365,7 +123,7 @@
                             <div class="row mb-3">
                                 <label for="department" class="col-sm-2 col-form-label">Department</label>
                                 <div class="col-sm-10">
-                                    <select name="department" id="department" class="form-control" required>
+                                    <select name="department" id="department" class="form-control">
                                         <option value="" disabled>Select Department</option>
                                         @foreach ($departments as $dept)
                                             <option value="{{ $dept->dept_name }}"
@@ -379,9 +137,9 @@
 
                             <!-- Head of Office -->
                             <div class="row mb-3">
-                                <label for="head_office" class="col-sm-2 col-form-label">Head of Office</label>
+                                <label for="head_office" class="col-sm-2 col-form-label">Approving Authority</label>
                                 <div class="col-sm-10">
-                                    <select name="head_office" id="head_office" class="form-control" required>
+                                    <select name="head_office" id="head_office" class="form-control">
                                         <option value="" disabled>Select Head</option>
                                         @foreach ($heads as $head)
                                             <option value="{{ $head->name }}"
