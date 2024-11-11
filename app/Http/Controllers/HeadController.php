ff0aa499a967d056cdf465338fe55c67ea104040
Romeo Tangpuz Jimenez <romeotjimenez@gmail.com>
2024-11-11 09:03:03 +0800

    sad

--- app/Http/Controllers/HeadController.php
+++ app/Http/Controllers/HeadController.php
@@ -39,7 +39,6 @@ class HeadController extends Controller
             'name' => 'required',
             'department' => 'required',
             'designation' => 'required',
-            'head_type' => ['nullable', 'required'],
             'email' => 'required',
             'password' => 'required',
             'phone_number' => 'required',
